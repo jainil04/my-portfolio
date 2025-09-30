a6e743cdcd55aa872a3241a8190cff91adcef8e9
Jainil Chauhan <33036617+jainil04@users.noreply.github.com>
2025-09-29 17:55:35 -0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import { HeroSection } from '@/components/custom/HeroSection';
+import { InnovationSection } from '@/components/custom/InnovationSection';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import '../styles/target-cursor.css';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Portfolio - Creative Digital Experiences',
+  description:
+    'Creative digital experiences that go beyond cookie cutter websites. Exceptional digital experiences for your brand.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <HeroSection />
+        <InnovationSection />
+        {children}
+      </body>
+    </html>
+  );
+}
